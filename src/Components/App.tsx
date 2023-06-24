8f7c8e515117c89bad45f20db4d66544e208548c
Dmitriy Vasnev <trance0id@yandex.ru>
2023-06-24 15:43:26 +0300

    styling: added themes

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,12 +1,16 @@
-import { Suspense } from "react";
+import { Suspense, useContext, useState } from "react";
 import { Link, Route, Routes } from "react-router-dom";
+import { Theme, ThemeContext } from "../theme/ThemeContext";
 import { AboutPageAsync } from "../pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "../pages/MainPage/MainPage.async";
-import './index.scss';
+import '../styles/index.scss';
+import { useTheme } from "../theme/useTheme";
 
 export const App = () => {
+    const { theme, toggleTheme } = useTheme();
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Toggle theme</button>
             <Link to={'/'}>Main Page</Link>
             <Link to={'/about'}>About Page</Link>
             <Suspense fallback={<div> Loading ... </div>}>
@@ -15,7 +19,6 @@ export const App = () => {
                     <Route path="/" element={<MainPageAsync />} />
                 </Routes>
             </Suspense>
-
         </div>
     );
 };

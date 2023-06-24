46ae1451a206611fc6b6447908800962f752e2ea
Dmitriy Vasnev <trance0id@yandex.ru>
2023-06-24 14:26:10 +0300

    routing: added routing, code splitting, lazy loading

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,10 +1,21 @@
-import { Counter } from "./Counter";
+import { Suspense } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import { AboutPageAsync } from "../pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "../pages/MainPage/MainPage.async";
 import './index.scss';
 
 export const App = () => {
     return (
         <div className="app">
-            <Counter />
+            <Link to={'/'}>Main Page</Link>
+            <Link to={'/about'}>About Page</Link>
+            <Suspense fallback={<div> Loading ... </div>}>
+                <Routes>
+                    <Route path="/about" element={<AboutPageAsync />} />
+                    <Route path="/" element={<MainPageAsync />} />
+                </Routes>
+            </Suspense>
+
         </div>
-    )
-}
+    );
+};

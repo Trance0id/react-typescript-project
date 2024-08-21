4a4f56128a95199c029e0a52e5645f699bf85f14
Dmitriy Vasnev <trance0id@yandex.ru>
2024-08-21 09:31:30 +0300

    delete bug button

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,11 +1,9 @@
-import { BugButton } from 'app/providers/ErrorBoundary';
 import { useTranslation } from 'react-i18next';
 
 export default function MainPage() {
     const { t } = useTranslation('main');
     return (
         <div>
-            <BugButton />
             {t('Главная страница')}
         </div>
     );
